dac5a2175b4c7c0618f278d4877d52d7a4032cc9
angelalucban07-bit <angelalucban07@gmail.com>
2026-06-05 04:20:44 +0800

    feat: base design and main function to test the dashboard and fan class if working properly.

--- lektrikfan/dashboard.py
+++ lektrikfan/dashboard.py
@@ -1,5 +1,13 @@
 class Dashboard:
 
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    RED = "\033[91m"
+    YELLOW = "\033[93m"
+    MAGENTA = "\033[95m"
+    WHITE = "\033[97m"
+    RESET = "\033[0m"
+
     def __init__(self):
         pass
 
---
+++ lektrikfan/main.py
@@ -0,0 +1,15 @@
+from fan import Fan
+from dashboard import Dashboard
+
+def main():
+
+    fan1 = Fan(Fan.FAST, 10, "yellow", True)
+    fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
+
+    dashboard = Dashboard()
+
+    dashboard.display_fan(fan1, 1)
+    dashboard.display_fan(fan2, 2)
+
+if __name__ == "__main__":
+    main()
